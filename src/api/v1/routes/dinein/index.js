76f514d52712be3e32c8b18ba2f2a12cbd448261
maryatul2323 <131692887+maryatul2323@users.noreply.github.com>
2023-05-28 19:43:12 +0700

    Add files via upload

--- src/api/v1/routes/dinein/index.js
+++ src/api/v1/routes/dinein/index.js
@@ -7,12 +7,14 @@ const dinein = require("./../../controllers/DineInControllers")
 router.post("/", dinein.postCustomer) 
 //menambahkan produk kekeranjang untuk dine in
 router.post("/add", dinein.addProduct)
+router.post("/addbulk", dinein.addBulkProduct)
 //menampilkan produk dikeranjang
 router.get("/keranjang", dinein.getKeranjang)
 //mengubah jumlah atau nilai lain di keranjang
 router.put('/:id', dinein.update)
 router.post('/checkout', dinein.addCheckout)
 router.get("/history", dinein.getHistory)
+router.get("/historyPesanan", dinein.getHistoryPesanan)
 router.get("/checkout/:id", dinein.detailCheckout)
 router.put("/checkout/:id", dinein.updateCheckout)
 router.get("/test", dinein.Test)
